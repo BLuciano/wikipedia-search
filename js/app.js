ea9cba713353661706173e27f7d86aed4369ead5
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2015-12-31 02:18:44 -0800

    Started working on API data call

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 (function(){
+	var url = "";
+	var wikiData = "";
+
 	$("#search p").click(function(){
 		$(this).addClass("animate-p");
 		$("#search span").html("x").addClass("animate-span");
@@ -8,6 +11,22 @@
 	$("#search span").click(function(){
 		$(this).html("|").removeClass("animate-span");
 		$("#search p").removeClass("animate-p");			
-		$("#search input").css("display", "none");
-		});
+		$("#search input").val("").css("display", "none");
+	});
+
+	$("#search form")[0].onkeypress = function(e){
+    	if (!e) e = window.event;
+    	var keyCode = e.keyCode || e.which;
+    	if (keyCode == '13'){
+    		if($("#search input").val().length > 0){
+    			searchWiki();
+			}
+    		$("#search input").val("");	
+      		return false;
+    	}
+  	}
+
+  	function searchWiki(){
+  		$("#search label").html("searching...");
+  	}
 })();
